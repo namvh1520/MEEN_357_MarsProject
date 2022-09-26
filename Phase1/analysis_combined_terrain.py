1e6ff0df303be9b92e5646df34165b70d85c4e4d
budgetdoughnut <namqvo123@gmail.com>
2022-09-26 11:31:07 -0500

    updated get_mass

--- Phase1/analysis_combined_terrain.py
+++ Phase1/analysis_combined_terrain.py
@@ -24,11 +24,14 @@ def fun1(x):
     
     return float(drive + grav + roll ) 
 
+def fun2(x):
+    return sf.F_net(np.array([x]), np.array([slope_sample]), rover, planet, Crr_sample)
+
 for i in range(N):
     for j in range(N):
         Crr_sample = float(CRR[i,j])
         slope_sample = float(SLOPE[i,j])
-        VMAX[i,j] = (sf.secant(fun1,1) * .3)
+        VMAX[i,j] = (sf.secant(fun1,1) * wheel['radius'])
         
 figure = plt.figure() 
 ax = Axes3D(figure, elev = 15, azim = 30) 
