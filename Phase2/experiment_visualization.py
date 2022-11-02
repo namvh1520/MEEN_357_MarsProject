a99182ee4a9b6b6d4dbc40e7f6f0f60083aa8792
hitchcock-tamu <112011136+hitchcock-tamu@users.noreply.github.com>
2022-11-02 11:26:43 -0500

    Updated with comments & plot titles

--- Phase2/experiment_visualization.py
+++ Phase2/experiment_visualization.py
@@ -7,12 +7,13 @@ import matplotlib.pyplot as plt
 def visualize_terrain():
     experiment = experiment1()[0]
         
-    alpha_fun = interp1d(experiment['alpha_dist'], experiment['alpha_deg'], kind = 'cubic', fill_value= 'extrapolate')
+    alpha_fun = interp1d(experiment['alpha_dist'], experiment['alpha_deg'], kind = 'cubic', fill_value= 'extrapolate') #cubic interpolation between the points in alphadeg as a function of alphadist
         
-    x = np.linspace(experiment['alpha_dist'][0], experiment['alpha_dist'][-1], 101)
+    x = np.linspace(experiment['alpha_dist'][0], experiment['alpha_dist'][-1], 101) #100 position points between first and last value of dist
     
-    plt.plot(experiment['alpha_dist'], experiment['alpha_deg'],'r*', x, alpha_fun(x))
+    plt.plot(experiment['alpha_dist'], experiment['alpha_deg'],'r*', x, alpha_fun(x)) #plotting
     plt.xlabel('Terrain Position (m)')
     plt.ylabel('Terrain Angle (deg)')
+    plt.title('Terrain Angle vs Terrain Position')
     
 visualize_terrain()
